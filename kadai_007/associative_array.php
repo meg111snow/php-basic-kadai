44edc705222b2528768913059bf11de7c79c8bf9
meg111snow <meg111snow@yahoo.co.jp>
2024-10-22 17:22:17 +1100

    kadai_007を追加

---
+++ kadai_007/associative_array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html lang="ja">
+
+<head>
+  <meta charset = "UTF-8">
+  <title>kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php
+      $vegetable = ['name' => 'onion', 'price' => 200, 'weight' => 160 ];
+      print_r($vegetable);
+    ?>
+  </p>
+</body>
+
+</html>
